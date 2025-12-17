96677cc9c8745df9717ae58e195c68787ad4fe32
senop333 <senop333@gmail.com>
2025-12-17 14:26:32 +0800

    comit 1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ app.get('/',(req, res) => {
     res.send('Helooo World');
 });
 
+app.get('/about',(req, res) => {
+    res.send('Helooo about');
+});
+
+app.get('/contact',(req, res) => {
+    res.send('Helooo contactt');
+});
+
+app.use("/", (req,res) => {
+    res.status(404)
+    res.send("Halaman tidak ditemukan");
+});
+
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
 });
